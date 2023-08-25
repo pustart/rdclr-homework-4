9e637dfc634fb4a266ce2bfffc5e92815e5d4d19
pustart <polaris9955@gmail.com>
2023-08-25 20:49:42 +0300

    feat: remove logic from main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,69 +1,19 @@
-import PostsController from './posts/posts.controller.mjs';
+import DomController from './dom/dom.controller.mjs';
 
-const container = document.getElementById('data-container');
-const loadMoreButton = document.getElementById('load-more');
-const pageSize = 10; // Количество элементов на странице
-let currentPage = 1;
-const postsController = new PostsController();
-
-// const fetchPosts = async (page) => {
-
-//   const response = await fetch(`https://dummyjson.com/posts?limit=${pageSize}&page=${page}`);
-//   const data = await response.json();
-//   return data.posts;
-// };
-
-const fetchPosts = async (page) => {
-
-  // const response = await fetch(`https://dummyjson.com/posts?limit=${pageSize}&page=${page}`);
-  const data = await postsController.getPosts(page, pageSize);
-  console.log(data);
-  return data.posts;
-};
-
-const renderData = (data) => {
-  data.forEach(item => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.textContent = item.title;
-    container.appendChild(card);
-  });
-};
-
-/* const renderData = (data) => {
-  if (data.hasOwnProperty('data') && Array.isArray(data.data)) {
-    data.data.forEach(item => {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.textContent = item.title;
-      container.appendChild(card);
-    });
-  } else {
-    console.error('Invalid data format:', data);
-  }
-};
- */
-
-const loadMoreData = async () => {
-  const data = await fetchPosts(currentPage);
-  renderData(data);
-  currentPage++;
-};
+const domController = new DomController();
+domController.init();
 
 const handleIntersection = (entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      loadMoreData();
+      domController.loadNewPosts();
     }
   });
 };
 
 const observer = new IntersectionObserver(handleIntersection, {
   root: null,
-  rootMargin: '0px',
-  threshold: 1.0
+  rootMargin: '100px',
+  threshold: 1
 });
 
-observer.observe(loadMoreButton);
-
-loadMoreButton.addEventListener('click', loadMoreData);
